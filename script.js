b0b4f299888733d3e250fefb32a7c1c62bf37dda
ezno02 <enzo.bmachado.e@gmail.com>
2025-10-15 17:22:58 -0300

    commit11 15-10-2025 (otimização na função getIframe)

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const coisasInvisiveis = document.getElementById('milCoisasInvisiveisLink')
     const recomecar = document.getElementById('recomecarLink')
     const goBack = document.getElementById('background-spotify')
+    const spotifyIframe = document.querySelector('#spotify-iframe iframe')
 
     // funçaõ dark/light mode
     function toggleTheme(theme) {
@@ -56,59 +57,32 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
-    function getIframe(card) {
-        switch (card) {
-            case 'atrasAlem':
-                body.classList.add('spotify-body')
-                spotifyDiv.innerHTML = '<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/6B2dACJHYyu5lO3ws2pBbp?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
-                break;
-            case 'mdqp':
-                body.classList.add('spotify-body')
-                spotifyDiv.innerHTML = '<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/6bjH9p0ZAdYbHqh8HFvspz?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
-                break;
-            case 'oTerno':
-                body.classList.add('spotify-body')
-                spotifyDiv.innerHTML = '<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/4woRdLXHCk9gTvr7BbtOKX?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
-                break;
-            case '66':
-                body.classList.add('spotify-body')
-                spotifyDiv.innerHTML = '<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/3iNtrqEywG7K98R4lYugx3?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
-                break;
-            case 'mci':
-                body.classList.add('spotify-body')
-                spotifyDiv.innerHTML = '<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/0icYpEwVxogZ6mCERLIVpa?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
-                break;
-            case 'recomecar':
-                body.classList.add('spotify-body')
-                spotifyDiv.innerHTML = '<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/6U97X9SjnkaBf94W5mkTs1?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
-                break;
-
-            case 'goBack':
-                body.classList.remove('spotify-body')
-                break;
-        }
+    function getIframe(srcIframe) {
+        spotifyIframe.src = srcIframe
+        body.classList.add('spotify-body')
     }
 
     atrasAlem.addEventListener('click', () => {
-        getIframe('atrasAlem')
+        getIframe('https://open.spotify.com/embed/album/6B2dACJHYyu5lO3ws2pBbp?utm_source=generator')
     })
     mdqp.addEventListener('click', () => {
-        getIframe('mdqp')
+        getIframe('https://open.spotify.com/embed/album/6bjH9p0ZAdYbHqh8HFvspz?utm_source=generator')
     })
     oTerno.addEventListener('click', () => {
-        getIframe('oTerno')
+        getIframe('https://open.spotify.com/embed/album/4woRdLXHCk9gTvr7BbtOKX?utm_source=generator')
     })
     meiaMeia.addEventListener('click', () => {
-        getIframe('66')
+        getIframe('https://open.spotify.com/embed/album/3iNtrqEywG7K98R4lYugx3?utm_source=generator')
     })
     coisasInvisiveis.addEventListener('click', () => {
-        getIframe('mci')
+        getIframe('https://open.spotify.com/embed/album/0icYpEwVxogZ6mCERLIVpa?utm_source=generator')
     })
     recomecar.addEventListener('click', () => {
-        getIframe('recomecar')
+        getIframe('https://open.spotify.com/embed/album/6U97X9SjnkaBf94W5mkTs1?utm_source=generator')
     })
     goBack.addEventListener('click', () => {
-        getIframe('goBack')
+        body.classList.remove('spotify-body')
+        spotifyIframe.src = ''
     })
 
 })
